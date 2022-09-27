4103080cf72ec7689efe812caca244c600255b32
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-09-27 17:18:46 -0300

    Commit
    -Add navbar bottom
    -CSS
    -Icons BS

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
-import './nav.css';
+import React, { useState } from "react";
+import "./nav.css";
+import {
+  BsGeoAltFill,
+  BsFillHouseFill,
+  BsFillPeopleFill,
+  BsExclamationCircleFill,
+} from "react-icons/bs";
 
-const Nav = () => {
+function Nav() {
+  const [selected, setSelected] = useState(0);
+
+  const menus = [
+    {
+      icon: <BsFillHouseFill size={25} />,
+      name: "Home",
+    },
+    {
+      icon: <BsGeoAltFill size={25} />,
+      name: "Map",
+    },
+    {
+      icon: <BsFillPeopleFill size={25} />,
+      name: "Comunity",
+    },
+    {
+      icon: <BsExclamationCircleFill size={25} />,
+      name: "Claims",
+    },
+  ];
+
   return (
-    <div>Nav</div>
-  )
+    <div className="navigation">
+      <ul>
+        {menus.map((val, index) => {
+          return (
+            <li
+              onClick={() => setSelected(index)}
+              key={index}
+              className={index === selected ? "active" : ""}
+            >
+              <div className="icon">{val.icon}</div>
+              <div className="name">{val.name}</div>
+            </li>
+          );
+        })}
+        <div className="menu-bg" />
+      </ul>
+    </div>
+  );
 }
-
-export default Nav
+export default Nav;
