2028584c1e14783f3a7278ee9a31b6bdd9ee613d
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-09-28 19:02:19 -0300

    Commit Card
    -Component create card and CSS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import BotNav from "./components/Nav/BotNav/BotNav";
+import Card from "./components/Card/Card";
 import TopNav from "./components/Nav/TopNav/TopNav";
-import Login from "./Pages/LoginChoice/Login/Login";
-import LoginChoice from "./Pages/LoginChoice/LoginChoice";
+/* import Login from "./Pages/LoginChoice/Login/Login";
+import LoginChoice from "./Pages/LoginChoice/LoginChoice"; */
 
 function App() {
   return (
     <>
-      <LoginChoice />
-      {/* <TopNav />
-      <BotNav /> */}
+      {/* <LoginChoice /> */}
+      <TopNav />
+      <Card />
+      <BotNav />
     </>
   );
 }
