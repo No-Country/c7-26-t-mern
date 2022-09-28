f3be1d175b77c1362c9cb5168c91a70f80badf7b
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-09-28 17:19:24 -0300

    Commit Rename logo
    -Rename "Logo" logo

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 import "./logo.css";
-import logoLogin from "../../Assets/img/logo.svg";
-
+import logoLogin from "../../assets/img/logo.svg";
 
 const Logo = () => {
-  return (
-    <img src={logoLogin} className="logo" alt="Aplication"></img>
-  );
+  return <img src={logoLogin} className="logo" alt="Aplication"></img>;
 };
 
 export default Logo;
