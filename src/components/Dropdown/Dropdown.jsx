99dac85c70041c1811d5e6bd879e568c729c7c83
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-09-29 17:42:05 -0300

    Commit
    -Save

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,81 +1,86 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './dropdown.css'
+import React, { useState, useRef, useEffect } from "react";
+import "./dropdown.css";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition } from "react-transition-group";
 import { AiOutlineUser } from "react-icons/ai";
 
 function Dropdown() {
-    return (
-        <Navbar>
-            <NavItem icon={<FontAwesomeIcon icon={faEllipsisV} className="dropdownIcon"/>}>
-                <DropdownMenu></DropdownMenu>
-            </NavItem>
-        </Navbar>
-    );
+  return (
+    <Navbar>
+      <NavItem
+        icon={<FontAwesomeIcon icon={faEllipsisV} className="dropdownIcon" />}
+      >
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
+    </Navbar>
+  );
 }
 
 function Navbar(props) {
-    return (
-        <nav className="navbar">
-            <ul className="navbar-nav">{props.children}</ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
 }
 
 function NavItem(props) {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    return (
-        <li className="navItem">
-            <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
-                {props.icon}
-            </a>
+  return (
+    <li className="navItem">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
 
-            {open && props.children}
-        </li>
-    );
+      {open && props.children}
+    </li>
+  );
 }
 
 function DropdownMenu() {
-    const [activeMenu, setActiveMenu] = useState('main');
-    const [menuHeight, setMenuHeight] = useState(null);
-    const dropdownRef = useRef(null);
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
 
-    useEffect(() => {
-        setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
-    }, [])
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, []);
 
-    function calcHeight(el) {
-        const height = el.offsetHeight;
-        setMenuHeight(height);
-    }
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
 
-    function DropdownItem(props) {
-        return (
-            <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-                <span className="icon-button">{props.leftIcon}</span>
-                {props.children}
-            </a>
-        );
-    }
-
+  function DropdownItem(props) {
     return (
-        <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
-
-            <CSSTransition
-                in={activeMenu === 'main'}
-                timeout={500}
-                classNames="menu-primary"
-                unmountOnExit
-                onEnter={calcHeight}>
-                <div className="menu">
-                    <DropdownItem
-                        leftIcon={<AiOutlineUser/>}>Profile</DropdownItem>
-                </div>
-            </CSSTransition>
-        </div>
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+      </a>
     );
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem leftIcon={<AiOutlineUser />}>Profile</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  );
 }
 
 export default Dropdown;
