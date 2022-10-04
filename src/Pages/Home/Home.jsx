f1c2b83687891088daaee637f81e5f0ab0579aa3
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-10-04 16:15:41 -0300

    Search Bar
    -Add component "Searchbar" MaterialUI

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import ButtonClaim from "../../components/ButtonClaim/ButtonClaim";
 import Card from "../../components/Card/Card";
 import TopNav from "../../components/Nav/BotNav/BotNav";
 import BotNav from "../../components/Nav/TopNav/TopNav";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Home = () => {
   return (
     <React.Fragment>
       <TopNav></TopNav>
       <BotNav></BotNav>
+      <SearchBar placeholder="Busca un reclamo" />
       <Card></Card>
       <Card></Card>
       <ButtonClaim></ButtonClaim>
