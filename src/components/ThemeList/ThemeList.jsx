7a6b4875c4daf1c655fce2743d989c40f4626165
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-10-05 18:04:51 -0300

    Commit
    -Changes and new component "SearchNewClaim"

--- src/components/ThemeList/ThemeList.jsx
+++ src/components/ThemeList/ThemeList.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import ThemeCard from "../ThemeCard/ThemeCard";
 import { themesData } from "../../assets/datas/themesData";
 import "./themelist.css";
-import TopNav from "../Nav/TopNav/TopNav";
-import SearchBar from "../SearchBar/SearchBar";
+import { Link } from "react-router-dom";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import SearchNewClaim from "../SearchNewClaim/SearchNewClaim";
 
 const ThemeList = () => {
   return (
     <>
-      <TopNav />
-      <SearchBar />
+      <div className="appNotificationMenu">
+        <Link to={"/home"} className="appNotificationIcon">
+          <AiOutlineArrowLeft />
+        </Link>
+        <span className="appNotificationTextMenu">Nuevo Reclamo</span>
+      </div>
+      <SearchNewClaim />
       <div className="themeCardContent">
         {themesData.map((card, idx) => (
           <ThemeCard url={card.url} title={card.title} key={idx} />
