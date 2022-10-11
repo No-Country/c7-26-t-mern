8f25fb20a589406ea8ee78be723a6a62b8a58f41
ANDRUPA <106835817+AndreUmbertReact@users.noreply.github.com>
2022-10-11 17:56:19 -0300

    publish backend branch
    backend branch just be created

--- src/components/HiddenCard/HiddenCard.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-export const HiddenCard = (component, visiblity = false) => {
-  const [visible, setVisiblity] = useState(() => visiblity);
-
-  return [visible ? component : null, () => setVisiblity((v) => !v)];
-};
-
-export default HiddenCard;
