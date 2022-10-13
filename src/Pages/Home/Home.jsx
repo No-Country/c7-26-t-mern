d848ed411fcb18331bee459420169d076609bab5
devleon <devleon00@gmail.com>
2022-10-13 16:39:07 -0500

    Fix components styles

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,26 @@ import Card from "../../Components/Card/Card";
 import TopNav from "../../Components/Nav/BotNav/BotNav";
 import BotNav from "../../Components/Nav/TopNav/TopNav";
 import SearchBar from "../../Components/SearchBar/SearchBar";
+import "./home.css";
 
 const Home = () => {
   return (
     <React.Fragment>
       <TopNav></TopNav>
       <BotNav></BotNav>
-      <SearchBar placeholder="Busca un reclamo" />
-      <Card title={"Titulo del reclamo"} subtitle={"subtitulo"}></Card>
+      <section className="sectionHome">
+        <SearchBar placeholder="Busca un reclamo" />
+
+        <div className="sectionHomeContainerCards">
+          <Card title={"Titulo del reclamo"} subtitle={"subtitulo"}></Card>
+          <Card title={"Titulo del reclamo"} subtitle={"subtitulo"}></Card>
+          <Card title={"Titulo del reclamo"} subtitle={"subtitulo"}></Card>
+          <Card title={"Titulo del reclamo"} subtitle={"subtitulo"}></Card>
+          <Card title={"Titulo del reclamo"} subtitle={"subtitulo"}></Card>
+          <Card title={"Titulo del reclamo"} subtitle={"subtitulo"}></Card>
+        </div>
+      </section>
+
       {/* <ButtonClaim></ButtonClaim> */}
     </React.Fragment>
   );
