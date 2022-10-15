e00a2da90c86919987d612efcbe46131faf8855f
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-10-14 21:07:28 -0500

    remove front

--- src/Components/PrimaryButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./primaryButton.css";
-
-const PrimaryButton = () => {
-  return <a className="primary-button">REGISTRATE</a>;
-};
-
-export default PrimaryButton;
--- src/Components/SecundaryButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./secundaryButton.css";
-
-const SecundaryButton = () => {
-  return <a className="secundary-button">INICIA SESION</a>;
-};
-
-export default SecundaryButton;
