1ab92760ca08321e402abf60784ef817f67914ee
Avril Umbert <106835817+AndreUmbertReact@users.noreply.github.com>
2022-10-15 23:16:35 -0300

    pictures router, service, controllers and middlewares
    that xD

--- backend/src/middlewares/exists.middlewares.js
+++ backend/src/middlewares/exists.middlewares.js
@@ -1,49 +1,64 @@
 // Models
-const { User } = require('../models/user.model');
+const { User } = require("../models/user.model");
 
 // Utils
-const { catchAsync } = require('../utils/catchAsync.util');
-const { AppError } = require('../utils/appError.util');
+const { catchAsync } = require("../utils/catchAsync.util");
+const { AppError } = require("../utils/appError.util");
 
 // services
-const institutionService = require('../services/institution.service');
+const institutionService = require("../services/institution.service");
+const pictureService = require("../services/picture.service");
 
 const userExists = catchAsync(async (req, _res, next) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const user = await User.findOne({
-		attributes: { exclude: ['password'] },
-		where: { id },
-	});
+  const user = await User.findOne({
+    attributes: { exclude: ["password"] },
+    where: { id },
+  });
 
-	// If user doesn't exist, send error message
-	if (!user) {
-		return next(new AppError('User not found', 404));
-	}
+  // If user doesn't exist, send error message
+  if (!user) {
+    return next(new AppError("User not found", 404));
+  }
 
-	// req.anyPropName = 'anyValue'
-	req.user = user;
-	next();
+  // req.anyPropName = 'anyValue'
+  req.user = user;
+  next();
 });
 
 const institutionExists = catchAsync(async (req, _res, next) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	const institution = await institutionService.searchById(id)
+  const institution = await institutionService.searchById(id);
 
-	// If institution doesn't exist, send error message
-	if (!institution) {
-		return next(new AppError('Institution not found', 404));
-	}
+  // If institution doesn't exist, send error message
+  if (!institution) {
+    return next(new AppError("Institution not found", 404));
+  }
 
-	// req.anyPropName = 'anyValue'
-	req.institution = institution;
-	next();
+  // req.anyPropName = 'anyValue'
+  req.institution = institution;
+  next();
 });
 
+const pictureExists = catchAsync(async (req, _res, next) => {
+  const { id } = req.params;
 
+  const picture = await pictureService.searchById(id);
+
+  // If picture doesn't exist, send error message
+  if (!picture) {
+    return next(new AppError("picture not found", 404));
+  }
+
+  // req.anyPropName = 'anyValue'
+  req.picture = picture;
+  next();
+});
 
 module.exports = {
-	userExists,
-	institutionExists
+  userExists,
+  institutionExists,
+  pictureExists,
 };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 // import routers
 const { institutionRouter } = require("./institution.routes");
+const { pictureRouter } = require("./picture.routes");
 const { userRouter } = require("./user.routes");
 
 const routersApp = express.Router();
 // route assignment
 routersApp
-   .use("/users", userRouter)
-   .use("/institutions", institutionRouter)
-
-module.exports = { routersApp }
+  .use("/users", userRouter)
+  .use("/institutions", institutionRouter)
+  .use("/pictures", pictureRouter);
+module.exports = { routersApp };
 
 //arquitectura MVC
