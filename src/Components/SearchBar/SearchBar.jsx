7e53b0af82b3879672b90ede35bbd29ece60cb6a
ANDRUPA <106835817+AndreUmbertReact@users.noreply.github.com>
2022-10-19 17:25:12 -0300

    searchbar filter
    searchbar filter was created, must be deleted

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -3,39 +3,71 @@ import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-import MicIcon from "@mui/icons-material/Mic";
+import { BsFilterSquareFill } from "react-icons/bs";
 import axios from "axios";
 
 import "./searchbar.css";
+import { useState } from "react";
 
 function SearchBar() {
+  const [show, setShow] = useState(false);
+
+  function setFilter(e) {
+    // console.log(e.target.attributes.option.value);
+    localStorage.setItem("filterSelected", e.target.attributes.option.value);
+  }
+
   return (
-    <Paper
-      component="form"
-      className="homeSearch"
-      sx={{
-        p: "2px 6px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        ml: 0,
-        mt: 0,
-        mb: 0,
-      }}
-    >
-      <IconButton sx={{ p: "10px" }} aria-label="menu">
-        <SearchIcon />
-      </IconButton>
-      <InputBase
-        sx={{ ml: 1, flex: 1, fontSize: 13 }}
-        placeholder="Busca un reclamo"
-        inputProps={{ "aria-label": "Busca un reclamo" }}
-      />
-      <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
-        <MicIcon />
-      </IconButton>
-    </Paper>
+    <div className="searchBarContainer">
+      <Paper
+        component="form"
+        className="homeSearch"
+        sx={{
+          p: "2px 6px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          ml: 0,
+          mt: 0,
+          mb: 0,
+        }}
+      >
+        <IconButton sx={{ p: "10px" }} aria-label="menu">
+          <SearchIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1, fontSize: 13 }}
+          placeholder="Busca un reclamo"
+          inputProps={{ "aria-label": "Busca un reclamo" }}
+        />
+        <IconButton
+          type="button"
+          sx={{ p: "10px" }}
+          aria-label="search"
+          onClick={() => setShow(!show)}
+        >
+          <BsFilterSquareFill />
+        </IconButton>
+      </Paper>
+      <div className={`${show ? "filterOptionsDivOn" : "filterOptionsDivOff"}`}>
+        <ul className="filterOptions">
+          <li option="categories" className="filterOption" onClick={setFilter}>
+            Categorias
+          </li>
+          <li
+            option="institutionns"
+            className="filterOption"
+            onClick={setFilter}
+          >
+            Instituciones
+          </li>
+          <li option="id" className="filterOption" onClick={setFilter}>
+            Numero de Reclamo
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
--- src/Pages/NewClaim/Filter.jsx
+++ src/Pages/NewClaim/Filter.jsx
@@ -2,10 +2,24 @@ import verified from "../../Assets/img/verified.svg";
 import { Link } from "react-router-dom";
 
 const Filter = (props) => {
+  function setInstitution(e) {
+    console.log(e.currentTarget.attributes.institution.value);
+    localStorage.setItem(
+      "institution",
+      e.currentTarget.attributes.institution.value
+    );
+  }
 
   return (
-    <Link to="/newClaim/category" state={{ Title: "Categoria", Institution: props}} >
-      <div className="filterContainer">
+    <Link
+      to="/newClaim/category"
+      state={{ Title: "Categoria", Institution: props }}
+    >
+      <div
+        onClick={setInstitution}
+        institution={props.text}
+        className="filterContainer"
+      >
         <div className="filterItemContainer">
           <img className="filterIconRounded" src={props.src} alt={props.alt} />
           <p className="filterText">{props.text}</p>
