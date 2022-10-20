febf884b2f2afa2f98ba0feb8abe692a39888c13
Emanuel Martinez <emanuelbeirac@gmail.com>
2022-10-19 21:47:43 -0300

    Commit
    -Add Link

--- src/Pages/MyClaims/MyClaims.jsx
+++ src/Pages/MyClaims/MyClaims.jsx
@@ -3,6 +3,7 @@ import TopNav from "../../Components/Nav/TopNav/TopNav";
 import BotNav from "../../Components/Nav/BotNav/BotNav";
 import TopToggle from "../../Components/TopToggle/TopToggle";
 import noResultsFoundIllustration from "../../Assets/img/noResultsFoundIllustration.svg";
+import { Link } from "react-router-dom";
 import "./myClaims.css";
 
 const MyClaims = () => {
@@ -18,7 +19,9 @@ const MyClaims = () => {
         />
         <p className="noResultsText">Aún no tienes ningún reclamo</p>
       </div>
-      <button className="addNewClaim">+ NUEVO RECLAMO</button>
+      <Link to="/newClaim/institution">
+        <button className="addNewClaim">+ NUEVO RECLAMO</button>
+      </Link>
       <BotNav />
     </div>
   );
