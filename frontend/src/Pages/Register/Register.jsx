1a97975a91c405994f2870cf065a8c6b2e66cfaa
ANDRUPA <106835817+AndreUmbertReact@users.noreply.github.com>
2022-10-21 01:04:04 -0300

    register
    ready

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import HeaderRegister from "./registerComponents/HeaderRegister/HeaderRegister";
 import LogoRounded from "../../Components/LogoRounded/LogoRounded";
@@ -7,8 +7,11 @@ import FormCredentials from "./registerComponents/FormCredentials/FormCredential
 import FormPersonal from "./registerComponents/FormPersonal/FormPersonal";
 
 import "./register.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Register = (props) => {
+const Register = () => {
+  const [claimmerDni, setClaimmerDni] = useState("");
   const [state, setState] = useState({
     step: 0,
     name: "",
@@ -18,6 +21,13 @@ const Register = (props) => {
     password: "",
     passwordRepited: "",
   });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (claimmerDni.length > 0) {
+      navigate("/login");
+    }
+  }, [claimmerDni]);
 
   const nextStep = () => {
     setState((state) => ({
@@ -41,11 +51,41 @@ const Register = (props) => {
     }));
   };
 
+  const registerFunction = async (e) => {
+    e.preventDefault();
+    await axios({
+      method: "post",
+      url: "http://localhost:4000/api/v1/claimmers",
+      data: {
+        name: state.name,
+        email: state.email,
+        password: state.password,
+        DNI: state.dni,
+        lastName: state.lastName,
+      },
+      headers: {
+        alg: "HS256",
+        typ: "JWT",
+      },
+    }).then(
+      ({ data: response }) => {
+        console.log(response);
+        setClaimmerDni(response.data.DNI);
+        console.log("Reclamante registrado correctamente");
+        window.localStorage.setItem("claimmer", response.data.DNI);
+      },
+      function error(params) {
+        console.error("Nombre o Contraseña incorrecta");
+        console.log(params);
+      }
+    );
+  };
+
   return (
     <section className="sectionRegister">
       <HeaderRegister
         to={state.step === 0 ? "/" : null}
-        onClick={state.step === 1 ? prevState : ""}
+        // onClick={state.step === 1 ? prevState : ""}
       />
       <section className="sectionRegisterContainer">
         <LogoRounded />
@@ -67,6 +107,7 @@ const Register = (props) => {
             prevState={prevState}
             handleChange={handleChange}
             values={state}
+            onClick={registerFunction}
           ></FormCredentials>
         )}
       </section>
--- frontend/src/Pages/Register/registerComponents/FormCredentials/FormCredentials.jsx
+++ frontend/src/Pages/Register/registerComponents/FormCredentials/FormCredentials.jsx
@@ -8,19 +8,18 @@ import "./formCredentials.css";
 const FormCredentials = (props) => {
   const next = (e) => {
     e.preventDefault();
-      axios.post('http://localhost:8000/signup', {
-        DNI: props.values.dni,
-        claimmerName: props.values.name,
-        claimmerLastname: props.values.lastName,
-        email: "pedro@gmail.com",
-        password: props.values.password
-      })
+    axios.post("http://localhost:8000/signup", {
+      DNI: props.values.dni,
+      claimmerName: props.values.name,
+      claimmerLastname: props.values.lastName,
+      email: "pedro@gmail.com",
+      password: props.values.password,
+    });
 
-      props.nextStep();
-    
+    props.nextStep();
   };
 
-  console.log(props.values)
+  // console.log(props.values);
 
   let buttonHandlerColor =
     props.values["email"] === "" ||
@@ -28,44 +27,48 @@ const FormCredentials = (props) => {
     props.values["passwordRepited"] === "";
 
   const emailValidation = () => {
-    let emailValue = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(props.values["email"])
-    if(emailValue || props.values["email"] === ""){
-      return true
+    let emailValue =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        props.values["email"]
+      );
+    if (emailValue || props.values["email"] === "") {
+      return true;
     }
-    return false
-  }
+    return false;
+  };
 
   const passwordValidation = () => {
-    let passwordValue = /^(?=.*[A-Z].*[a-z]).{8,16}$/.test(props.values["password"])
-    if (passwordValue || props.values["password"] === ""){
-      return true
+    let passwordValue = /^(?=.*[A-Z].*[a-z]).{8,16}$/.test(
+      props.values["password"]
+    );
+    if (passwordValue || props.values["password"] === "") {
+      return true;
     }
-    return false
-  }
+    return false;
+  };
 
   const samePassword = () => {
-    const password = props.values["password"]
-    const repeatPassword = props.values["passwordRepited"]
-    if (repeatPassword === password || repeatPassword === ""){
-      return true
+    const password = props.values["password"];
+    const repeatPassword = props.values["passwordRepited"];
+    if (repeatPassword === password || repeatPassword === "") {
+      return true;
     }
-    return false
-  }
+    return false;
+  };
 
   const buttonDisable = () => {
-    let email = emailValidation()
-    let password = passwordValidation()
-    let repeatPassword = samePassword()
+    let email = emailValidation();
+    let password = passwordValidation();
+    let repeatPassword = samePassword();
 
-    if (email && password && repeatPassword && !buttonHandlerColor){
-      return true
+    if (email && password && repeatPassword && !buttonHandlerColor) {
+      return true;
     }
-    return false
-  }
+    return false;
+  };
 
-  console.log(buttonDisable())
-  console.log(buttonHandlerColor)
-
+  // console.log(buttonDisable());
+  // console.log(buttonHandlerColor);
 
   return (
     <React.Fragment>
@@ -76,7 +79,9 @@ const FormCredentials = (props) => {
         onChange={props.handleChange("email")}
         defaultValue={props.values.email}
       />
-      {!emailValidation() && <span>*Por favor, introduzca un email valido</span>}
+      {!emailValidation() && (
+        <span>*Por favor, introduzca un email valido</span>
+      )}
 
       <InputField
         eventBlur={passwordValidation}
@@ -85,9 +90,12 @@ const FormCredentials = (props) => {
         onChange={props.handleChange("password")}
         defaultValue={props.values.password}
       />
-      {!passwordValidation() && <span>*La contraseña debe contener al menos:
-         Una letra mayuscula, Una letra minuscula y tener una longitud de 8 a 16 caracteres
-         </span>}
+      {!passwordValidation() && (
+        <span>
+          *La contraseña debe contener al menos: Una letra mayuscula, Una letra
+          minuscula y tener una longitud de 8 a 16 caracteres
+        </span>
+      )}
 
       <InputField
         type={"password"}
@@ -103,6 +111,7 @@ const FormCredentials = (props) => {
         color="#F0F0F0"
         to="/home"
         disabled={!buttonDisable() ? "false" : ""}
+        onClick={props.onClick}
       />
     </React.Fragment>
   );
